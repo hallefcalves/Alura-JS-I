845c490d416958f78f1b5f5c041b4bdcd1dc430c
Hallef Alves <hallef28@gmail.com>
2021-08-06 16:48:59 -0300

    Primeiras 2 aulas

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,12 @@
+//Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Hallef";
+
+console.log(idade);
+
+//Não fazer
+//mes = "Janeiro";
+
